914e8b52892af455dd64d4e16813de429e140615
mohip patel <patelmohip9@gmail.com>
2022-06-01 11:32:37 +0530

    Demo testing

---
+++ functions.php
@@ -0,0 +1,76 @@
+<?php
+
+
+function enqueue_media() {
+    wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/widget-js.js', array(), '1.0.0', true );
+    wp_enqueue_script('media-upload');
+    wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+    wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.4.1.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot3','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_media();
+
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_media' );
+
+
+add_action( 'wp_enqueue_scripts', 'my_plugin_add_stylesheet' );
+function my_plugin_add_stylesheet() {
+    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
+}
+
+
+
+//Add a custom user role
+
+$result = add_role( 'client', __('Client' ), array(
+    'read' => true, 
+    'edit_posts' => true,
+    'edit_pages' => true, 
+    'edit_others_posts' => true, 
+    'create_posts' => true, 
+    'manage_categories' => true, 
+    'publish_posts' => true,
+    'edit_themes' => false,
+    'install_plugins' => false, 
+    'update_plugin' => false,
+    'update_core' => false 
+) );
+
+$result = add_role( 'contractor', __('Contractor' ), array(
+    'read' => true, 
+    'edit_posts' => true,
+    'edit_pages' => true, 
+    'edit_others_posts' => true, 
+    'create_posts' => true, 
+    'manage_categories' => true, 
+    'publish_posts' => true,
+    'edit_themes' => false,
+    'install_plugins' => false, 
+    'update_plugin' => false,
+    'update_core' => false 
+) );
+
+
+add_action('wp_ajax_contact_us', 'ajax_contact_us');
+function ajax_contact_us(){
+    $arr=[];
+    wp_parse_str($_POST['contact_us'],$arr);
+    global $wpdb;
+    global $table_prefix;
+    $table = $table_prefix.'contact_us';
+    $result = $wpdb->insert($table,[
+        "name" => $arr['name'],
+        "email" => $arr['email'],
+        "phone" => $arr['phone'],
+        "address" => $arr['address'],
+        "bname" => $arr['bname'],
+        "bnumber" => $arr['bnumber']
+    ]);
+    if($result>0){
+        wp_send_json_success("Data inserted");
+    }else{
+        wp_send_json_error("Please try again");
+    }
+}
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php get_header(); ?>
+
+
+<div class="container">
+	<div class="service_class">
+		<div class="header_class">
+			<h2>Register Yourself Here:  </h2>
+		</div>
+		<ul class="nav nav-tabs navigation_tabs">
+		    <li class="active"><a data-toggle="tab" href="#home">Client</a></li>
+		    <li><a data-toggle="tab" href="#menu1">Contractor</a></li>
+		 </ul>
+
+		 <div class="tab-content">
+		    <div id="home" class="tab-pane fade in active">
+		    	<h3>Client</h3>
+		      	<form id="frmContactUs">
+					<div class="form-group">
+					    <label for="Name">First Name</label>
+					    <input type="text" class="form-control" name="name" id="name" placeholder="Name" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Email">Email</label>
+					    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Phone">Phone No.</label>
+					    <input type="tel" class="form-control" name="phone" id="phone" placeholder="Phone" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Address">Address</label>
+					    <input type="text" class="form-control" name="address" id="address" placeholder="Address" required/>
+					</div>
+					<div class="form-group">
+					    <input type="submit" class="form-control" name="submit" id="submit" placeholder="Submit" >
+					</div>
+					<div id="result_msg">
+						
+					</div>
+				</form>
+		    </div>
+		    <div id="menu1" class="tab-pane fade">
+		      	<h3>Contractor</h3>
+		      	<form id="contractorform">
+					<div class="form-group">
+					    <label for="Name">First Name</label>
+					    <input type="text" class="form-control" name="name" id="name" placeholder="Name" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Email">Email</label>
+					    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Phone">Phone No.</label>
+					    <input type="tel" class="form-control" name="phone" id="phone" placeholder="Phone" required/>
+					</div>
+					<div class="form-group">
+					    <label for="Address">Address</label>
+					    <input type="text" class="form-control" name="address" id="address" placeholder="Address" required/>
+					</div>
+					<div class="form-group">
+					    <label for="BName">Business Name</label>
+					    <input type="text" class="form-control" name="bname" id="bname" placeholder="Business Name" required/>
+					</div>
+					<div class="form-group">
+					    <label for="BNumber">Business Number</label>
+					    <input type="tel" class="form-control" name="bnumber" id="bnumber" placeholder="Business Number" required/>
+					</div>
+					<div class="form-group">
+					    <input type="submit" class="form-control" name="submit" id="submit" placeholder="Submit" >
+					</div>
+					<div id="result_msg">
+						
+					</div>
+				</form>
+		    </div>
+		 </div>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+	jQuery(document).ready(function(){
+		jQuery('#frmContactUs').submit(function(){
+			event.preventDefault();
+			jQuery('#result_msg').html('');
+			var link = "<?php echo admin_url('admin-ajax.php') ?>";
+			var form = jQuery('#frmContactUs').serialize();
+			var formData = new FormData;
+			formData.append('action', 'contact_us');
+			formData.append('contact_us', form);
+			jQuery.ajax({
+				url:link,
+				data:formData,
+				processData:false,
+				contentType:false,
+				type:'post',
+				success:function(result){
+					if(result.success==true){
+						jQuery('#frmContactUs')[0].reset();
+					}
+					jQuery('#result_msg').html('<h3><span class="'+result.success+'">'+result.data+'</span></h3>')
+				}
+			});
+		});
+	});
+</script>
+
+<?php get_footer(); ?>
